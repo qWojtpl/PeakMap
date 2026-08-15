4c1d1d3b0180a90db91b59915c4911664c3e9490
qWojtpl <wojtoo24@gmail.com>
2026-08-15 14:45:41 +0200

    Map zoom & loader
    - Added loader
    - Added zoom

--- WebApp/script.js
+++ WebApp/script.js
@@ -113,13 +113,101 @@ function nextLevel() {
 }
 
 function loadLevel(level) {
-    if(level < 0 || level > maxLevel) {
+    if(level < 0) {
+        loadLevel(maxLevel);
         return;
     }
+    if(level > maxLevel) {
+        loadLevel(0);
+        return;
+    }
+    zoom = 1;
+    zoomTop = 0;
+    zoomLeft = 0;
+    updateZoom();
+
     removeLuggage();
     removeBelltowers();
     currentLevel = level;
-    document.getElementById("map").setAttribute("src", "./data/level_" + level + ".png");
-    document.getElementById("luggage-frame").src = "./data/level_" + level + "_luggage.json"; 
-    document.getElementById("belltowers-frame").src = "./data/level_" + level + "_belltowers.json"; 
+    
+    let map = document.getElementById("map");
+    map.loading = true;
+    let newImage = new Image();
+    newImage.onload = function() {
+        requestAnimationFrame(() => {
+            map.src = this.src;
+            document.getElementById("luggage-frame").src = "./data/level_" + level + "_luggage.json"; 
+            document.getElementById("belltowers-frame").src = "./data/level_" + level + "_belltowers.json"; 
+            requestAnimationFrame(() => {
+                map.loading = false;
+            });
+        });
+    }
+    newImage.src = "./data/level_" + level + ".png";
+}
+
+// Zoom
+
+let zoom = 1;
+let zoomTop = 0;
+let zoomLeft = 0;
+let dragStartX = 0;
+let dragStartY = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("container");
+    document.addEventListener("wheel", (e) => {
+        if(e.deltaY > 0) {
+            if(zoom > 1) {
+                zoom -= 0.5;
+            } else {
+                zoomTop = 0;
+                zoomLeft = 0;
+            }
+        } else if(e.deltaY < 0) {
+            if(zoom < 19.5) {
+                zoom += 0.5;
+            }
+        }
+        updateZoom();
+    });
+    container.addEventListener("dragstart", (e) => {
+        dragStartX = e.screenX;
+        dragStartY = e.screenY;
+    });
+    container.addEventListener("dragover", (e) => {
+        if(zoom == 1) {
+            return;
+        }
+        let deltaX = (dragStartX - e.screenX) / 10;
+        zoomLeft -= deltaX;
+        
+        let deltaY = (dragStartY - e.screenY) / 10;
+        zoomTop -= deltaY;
+
+        updateZoom();
+    });
+});
+
+function updateZoom() {
+    const container = document.getElementById("container");
+    container.style.transform = `scale(${zoom})`;
+    container.style.top = `${zoomTop}px`;
+    container.style.left = `${zoomLeft}px`;
+
+    let points = document.getElementsByClassName("point");
+    let size = 3 - zoom / 2;
+    if(size < 0.3) {
+        size = 1;
+    }
+    for(let i = 0; i < points.length; i++) {
+        points[i].style.width = `${size}vmin`;
+        points[i].style.height = `${size}vmin`;
+        points[i].style.borderWidth = `${size / 10}vmin`;
+    }
+    if(zoom == 1) {
+        document.getElementById("settings").style.position = "absolute";
+    } else {
+        document.getElementById("settings").style.position = "fixed";
+    }
 }
