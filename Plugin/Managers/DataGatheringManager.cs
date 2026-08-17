094013ebffbd922dc49bb5d03bd9b5f6c57c8a72
qWojtpl <wojtoo24@gmail.com>
2026-08-17 20:00:32 +0200

    Actions deploy test

--- Plugin/Managers/DataGatheringManager.cs
+++ Plugin/Managers/DataGatheringManager.cs
@@ -1,13 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Newtonsoft.Json;
-using Peak;
 using PeakMap.Objects;
 using UnityEngine;
 using Zorro.Core;
-using Object = UnityEngine.Object;
 
 namespace PeakMap.Managers;
 
